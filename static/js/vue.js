f96d1ef85d490b6c20b880caecc0e1e9d54fa458
hufei <hufei@alfa-media.cn>
2019-04-22 20:24:16 +0800

    vote api 前端支持

--- static/js/vue.js
+++ static/js/vue.js
@@ -4,7 +4,7 @@ var app = new Vue({
 		message: 'Hello Vue!'
 	},
 	methods: {
-		submitUrl: function () {
+		submitUrl: function() {
 			var url = e("#url-text").value
 			var author = e("#author").value
 			var data = {
@@ -19,6 +19,40 @@ var app = new Vue({
                 } else {
                     alert("not ok")
                 }
+			})
+		},
+
+		voteOk: function(event) {
+			var data = {
+				"img_id": parseInt(event.target.parentElement.dataset.imgid),
+				"type": 1,
+			}
+			var path = `/api/vote`
+			ajax("PUT", path, data, function(r) {
+				r = JSON.parse(r)
+				if (r.code == 0) {
+					alert("投票成功")
+				} else {
+					alert(r.code)
+				}
+
+			})
+		},
+
+		voteNo: function(event) {
+			var data = {
+				"img_id": parseInt(event.target.parentElement.dataset.imgid),
+				"type": 0,
+			}
+			var path = `/api/vote`
+			ajax("PUT", path, data, function(r) {
+				r = JSON.parse(r)
+				if (r.code == 0) {
+					alert("投票成功")
+				} else {
+					alert(r.code)
+				}
+
 			})
 		}
 	}
