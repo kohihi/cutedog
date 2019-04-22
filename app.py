c79e1c87bddafe1e93a7bd02b2e5bf18d98533e8
hufei <hufei@alfa-media.cn>
2019-04-22 16:40:09 +0800

    去掉 flask-admin

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from flask_admin import Admin
 
 
 def create_app():
     r = Flask(__name__)
     r.secret_key = "^&*jh_jhf_d_kas("
 
-    r.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     r.config['MONGODB_SETTINGS'] = {
         'db': 'Wang',
         'host': '127.0.0.1',
@@ -20,5 +18,3 @@ def create_app():
 
 
 app = create_app()
-
-admin = Admin(app, name='microblog', template_mode='bootstrap3')
